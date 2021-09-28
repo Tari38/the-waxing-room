782a160dc1d1c8ffe3e49233613bd03d126ffd56
Samantha <51409073+Tari38@users.noreply.github.com>
2021-09-28 12:55:21 +0100

    removed card component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,41 +15,33 @@ export default function Home() {
     
     <div className="homepage-container"> 
       <Jumbotron className="jumbotron">
-        
-          <img className="twrLogo" src={twrLogo} alt="the waxing room logo"></img>
-        
+        <img className="twrLogo" src={twrLogo} alt="the waxing room logo"></img>
       </Jumbotron>
       
       <Row className="row" col="2">
         <Col className="home-col">
-        
             <h3>Services</h3>
             <p style={{ fontFamily: `"Montserrat", sans-serif`}}>We have services for men & women.</p>
-            <Button className="card-btn" href="/components/pages/services" style={{ backgroundImage: `url(${brushedMetal})` }}>View Services</Button>
-          
+            <Button className="home-card-btn" href="/components/pages/services" style={{ backgroundImage: `url(${brushedMetal})` }}>View Services</Button>
         </Col>
-        <Col className="home-col">
         
+        <Col className="home-col">
             <h3>Contact Us</h3>
             <p style={{ fontFamily: `"Montserrat", sans-serif`}}>Make an appointment or need advice?</p>
-            <Button className="card-btn" href="/components/pages/contact" style={{ backgroundImage: `url(${brushedMetal})` }}>Contact Us</Button>
-          
+            <Button className="home-card-btn" href="/components/pages/contact" style={{ backgroundImage: `url(${brushedMetal})` }}>Contact Us</Button>
         </Col>
        
         <Col className="home-col">
-          
             <h3>About Us</h3>
             <p style={{ fontFamily: `"Montserrat", sans-serif`}}>Opening hours, location, Elaine & Perron Rigot.</p>
-            <Button className="card-btn" href="/components/pages/about" style={{ backgroundImage: `url(${brushedMetal})` }}>Find Out More</Button>
-        
+            <Button className="home-card-btn" href="/components/pages/about" style={{ backgroundImage: `url(${brushedMetal})` }}>Find Out More</Button>
         </Col>
+        
         <Col className="home-col">
             <h3>More</h3>
             <p style={{ fontFamily: `"Montserrat", sans-serif`}}>Waxing etiquette, aftercare, our shop and reviews.</p>
-            <Button className="card-btn" href="/components/pages/products" style={{ backgroundImage: `url(${brushedMetal})` }}>View More</Button>
-          
+            <Button className="home-card-btn" href="/components/pages/products" style={{ backgroundImage: `url(${brushedMetal})` }}>View More</Button>
         </Col>
-      
       </Row>
     </div>
     </>
